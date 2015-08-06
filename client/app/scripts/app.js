e4b90fbd1235cf2a4b5b670055d06a9a03e1bf59
Johnny Vu <jvu@guthy-renker.com>
2015-08-06 15:50:43 -0700

    inital commit grunt foundation yo scaffolding

---
+++ client/app/scripts/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngCookies',
+    'ngRoute',
+    'ngSanitize',
+    'restangular'
+  ])
+  .config(function ($routeProvider, RestangularProvider) {
+    // Set the base URL for Restangular.
+    RestangularProvider.setBaseUrl('http://localhost:3000');
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/movies', {
+        templateUrl: 'views/movies.html',
+        controller: 'MoviesCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .factory('MovieRestangular', function(Restangular) {
+  return Restangular.withConfig(function(RestangularConfigurer) {
+    RestangularConfigurer.setRestangularFields({
+      id: '_id'
+    });
+  });
+})
+  .factory('Movie', function(MovieRestangular) {
+  return MovieRestangular.service('movie');
+});
