b34ea3a36ec92241eb130031aa6dc34733dbeff4
Johnny Vu <jvu@guthy-renker.com>
2015-08-08 07:23:07 -0700

    add crud funtionality

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -34,6 +34,26 @@ angular
         templateUrl: 'views/movies.html',
         controller: 'MoviesCtrl'
       })
+      .when('/create/movie', {
+        templateUrl: 'views/movie-add.html',
+        controller: 'MovieAddCtrl',
+        controllerAs: 'movieAdd'
+      })
+      .when('/movie/:id', {
+        templateUrl: 'views/movie-view.html',
+        controller: 'MovieViewCtrl',
+        controllerAs: 'movieView'
+      })
+      .when('/movie/:id/delete', {
+        templateUrl: 'views/movie-delete.html',
+        controller: 'MovieDeleteCtrl',
+        controllerAs: 'movieDelete'
+      })
+      .when('/movie/:id/edit', {
+        templateUrl: 'views/movie-edit.html',
+        controller: 'MovieEditCtrl',
+        controllerAs: 'movieEdit'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ client/app/scripts/controllers/movie-delete.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieDeleteCtrl
+ * @description
+ * # MovieDeleteCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+  .controller('MovieDeleteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
